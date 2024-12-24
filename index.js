438c77a07462a5c6057da3fa9be2ee4cd151e914
hkprasadam <hemanthkumarprasadam48@gmail.com>
2024-12-24 10:56:59 +0530

    add for loop

---
+++ index.js
@@ -0,0 +1,16 @@
+debugger;
+// for(let i=1;i<=20;i++){
+//     console.log(i)
+// }
+// for(i=0;i<=50;i=i+5){
+//     console.log(i)
+// }
+// ----for increment-------
+// for(i=0;i<=50;i++)(
+//     console.log(`5x${i}=${i*5}`)
+// )
+// -----for decreement------
+for(i=50;i>=1;i--){
+    console.log(`5x${i}=${i*5}`)
+}
+
